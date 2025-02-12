2914b8436bfaf9088b00e4435f4eb0416973dad8
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-02-12 15:50:33 +0300

    commit changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
+  const posts = [
+    {id: 1, title:"The WET Codebase", date:"February 12, 2025", preview:"Come waste your time with me."},
+    {id: 2, title:"Goodbye, clean code", date:"February 12, 2025", preview:"Let clean code guide you. Then let it go."},
+    {id: 3, title:"My Decade in Review", date:"February 12, 2025", preview:"Apersonal reflection."}
+  ]
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name ="Overreacted" />
+      <About image="" about={
+        <>
+        Personal blog by <a href="https://example.com">Nuria Mohamed.</a>
+      <br />
+      I explain with words and code.</>
+      } />
+
+      <ArticleList posts={posts} />
+
     </div>
   );
 }
